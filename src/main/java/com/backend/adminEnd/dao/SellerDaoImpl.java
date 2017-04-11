3d2d5507a4bcbc1d6aae9a7afe07b49bae39496f
makoto <kevin_dante@foxmail.com>
2017-04-11 16:55:09 +0800

    Update

--- src/main/java/com/backend/adminEnd/dao/SellerDaoImpl.java
+++ src/main/java/com/backend/adminEnd/dao/SellerDaoImpl.java
@@ -1,7 +1,14 @@
 package com.backend.adminEnd.dao;
 
+import java.util.List;
+import com.backend.adminEnd.model.SellerEntity;
 /**
  * Created by kevin on 2017/4/11.
  */
-public class SellerDaoImpl {
+public interface SellerDaoImpl {
+    public void addSeller(SellerEntity seller);
+    public void updateSeller(SellerEntity seller);
+    public List<SellerEntity> listSeller();
+    public SellerEntity getSellerById(String sellerId);
+    public void removeSeller(String sellerId);
 }

bbc7a5f8b07a634295c800cd5be8d9189b2541d4
makoto <kevin_dante@foxmail.com>
2017-04-11 17:01:56 +0800

    Update

--- src/main/java/com/backend/adminEnd/dao/GoodsDaoImpl.java
+++ src/main/java/com/backend/adminEnd/dao/GoodsDaoImpl.java
@@ -7,8 +7,6 @@ import com.backend.adminEnd.model.GoodsEntity;
  * Created by kevin on 2017/4/11.
  */
 public interface GoodsDaoImpl {
-    public void addGoods(GoodsEntity goods);
-    public void updateGoods(GoodsEntity goods);
     public List<GoodsEntity> listGoods();
     public GoodsEntity getGoodsByGoodsId(String goodsId);
     public void removeGoods(String goodsId);
--- src/main/java/com/backend/adminEnd/dao/OrderDaoImpl.java
+++ src/main/java/com/backend/adminEnd/dao/OrderDaoImpl.java
@@ -1,7 +1,12 @@
 package com.backend.adminEnd.dao;
+import java.util.List;
+import com.backend.adminEnd.model.OrderEntity;
 
 /**
  * Created by kevin on 2017/4/11.
  */
-public class OrderDaoImpl {
+public interface OrderDaoImpl {
+    public void removeOrder(String orderId);
+    public List<OrderEntity> listOrder();
+    public OrderEntity getOrderById(String orderId);
 }

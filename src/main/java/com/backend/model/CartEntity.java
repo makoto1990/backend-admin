0df79fecf67177695f590fef734c59ab618e5d5e
makoto <kevin_dante@foxmail.com>
2017-05-10 21:51:54 +0800

    Finally registerable. It sucks.

---
+++ src/main/java/com/backend/model/CartEntity.java
@@ -0,0 +1,69 @@
+package com.backend.model;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by kevin on 2017/5/10.
+ */
+@Entity
+@Table(name = "Cart", schema = "dbo", catalog = "onlineMarket")
+public class CartEntity {
+    private String userId;
+    private String goodsId;
+    private Integer count;
+
+    @Basic
+    @Column(name = "userID", nullable = false, length = 12)
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @Basic
+    @Column(name = "goodsID", nullable = true, length = 12)
+    public String getGoodsId() {
+        return goodsId;
+    }
+
+    public void setGoodsId(String goodsId) {
+        this.goodsId = goodsId;
+    }
+
+    @Basic
+    @Column(name = "count", nullable = true)
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CartEntity that = (CartEntity) o;
+
+        if (userId != null ? !userId.equals(that.userId) : that.userId != null) return false;
+        if (goodsId != null ? !goodsId.equals(that.goodsId) : that.goodsId != null) return false;
+        if (count != null ? !count.equals(that.count) : that.count != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = userId != null ? userId.hashCode() : 0;
+        result = 31 * result + (goodsId != null ? goodsId.hashCode() : 0);
+        result = 31 * result + (count != null ? count.hashCode() : 0);
+        return result;
+    }
+}

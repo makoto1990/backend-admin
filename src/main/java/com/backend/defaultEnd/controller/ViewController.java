29eba2994122f19d06bb9b429b27f65ec572a92a
makoto <kevin_dante@foxmail.com>
2017-05-16 19:50:07 +0800

    Modified Register

--- src/main/java/com/backend/defaultEnd/controller/ViewController.java
+++ src/main/java/com/backend/defaultEnd/controller/ViewController.java
@@ -4,10 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import com.backend.model.*;
@@ -37,7 +35,7 @@ public class ViewController {
     }
 
     @RequestMapping(value = "/register",method = RequestMethod.GET)
-    public String toRegister(Model model){
+    public String toRegister(@RequestParam Model model){
         model.addAttribute("user",new UserEntity());
         return "register";
     }
--- src/main/webapp/WEB-INF/page/default/index.jsp
+++ src/main/webapp/WEB-INF/page/default/index.jsp
@@ -3,7 +3,7 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ taglib uri="http://www.springframework.org/tags" prefix="spring" %>
 <%@ taglib uri="http://www.springframework.org/tags/form" prefix="form" %>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html>
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
@@ -26,7 +26,7 @@ ${sessionScope.message}
         </label>
         <div>
             <button class="button" type="submit">登录</button>
-            <button class="button"  onclick="window.location.href='/register';">注册</button>
+            <button class="button" onclick="window.location.href='/register'">注册</button>
         </div>
 
     </form:form>

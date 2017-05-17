7a6d5e3b08a56f05a9da490c9a5d8802c4550525
makoto <kevin_dante@foxmail.com>
2017-05-17 21:00:15 +0800

    modified register

--- src/main/java/com/backend/defaultEnd/service/RegisterService.java
+++ src/main/java/com/backend/defaultEnd/service/RegisterService.java
@@ -48,6 +48,11 @@ public class RegisterService {
         user.setUserId(userID);
         user.setRegisterDate(new Timestamp(System.currentTimeMillis()));
         try{
+            if(this.dao.getUserByUsername(user.getUserName())!=null)return false;
+        } catch (Exception e) {
+            return false;
+        }
+        try{
             addUser(user);
         }catch (Exception e){
             return false;
